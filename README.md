cfe1402ca4aa0199914013b4f27269c4dbf3462d
Zaki Shaikh <zaki-shaikh@outlook.com>
2020-12-08 00:19:09 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NodeJS Droplet API
+
+Hello World! First ever Digital Ocean Droplet using NodeJS Express.
+
+Commands:
+```
+npm i express
+npm i nodemon -D
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World! First ever Digital Ocean Droplet using NodeJS Express.');
+})
+
+app.listen(port, () => {
+    console.log(`Droplet running on ${port}`)
+});
